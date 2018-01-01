20f2361c61a5fadf49617f848cd4f036547a3d19
Theo Okelola <tokelola88@gmail.com>
2018-01-01 10:34:34 -0600

    initial commit

---
+++ battleship/arrays.js
@@ -0,0 +1,7 @@
+var scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54];
+
+var solution2 = scores[3];
+var solution3 = scores[4];
+
+var numScores = scores.length;
+alert("Solution 2 produced " + solution2 + " bubbles and solution 3 produced " + solution3 + " bubbles.");
